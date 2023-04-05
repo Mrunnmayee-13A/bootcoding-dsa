abf4361c7d4fa12199c2ceccbe13f8be598354b0
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-04-05 18:50:09 +0530

    CopyEven Array

---
+++ src/main/java/com/bootcoding/dsa/array/CopyEvenArray.java
@@ -0,0 +1,29 @@
+package com.bootcoding.dsa.array;
+
+public class CopyEvenArray {
+    public static void main(String[] args) {
+        int a[]={14, 67, 88, 69, 171, 28};
+        int count =0;
+        for(int i=0; i<a.length; i++){
+            if(a[i]%2==0) {
+                count ++;
+            }
+        }
+        int b[]= new int[count];
+        int index=0;
+        for (int i=0; i<a.length;i++) {
+            if (a[i] % 2 == 0) {
+                b[index++] = a[i];
+            }
+        }
+        System.out.println("Old Array");
+        for(int i=0;i< a.length; i++) {
+            System.out.print(a[i]+" ");
+        }
+        System.out.println("\nNew Array");
+        for (int i=0; i<b.length; i++){
+            System.out.print(b[i]+" ");
+        }
+        }
+    }
+
