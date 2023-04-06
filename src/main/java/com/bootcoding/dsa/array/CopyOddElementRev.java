be748993b453af97b5924ab17005279497af0271
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-04-06 19:24:26 +0530

    Copy Odd Element Array

---
+++ src/main/java/com/bootcoding/dsa/array/CopyOddElementRev.java
@@ -0,0 +1,32 @@
+package com.bootcoding.dsa.array;
+
+public class CopyOddElementRev {
+    public static void main(String[] args) {
+        int input[] = {3, 45, 56, 76, 79, 57};
+        int count = 0;
+
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] % 2 != 0) {
+            }
+        }
+        int[] output = new int[count];
+        int index = output.length - 1;
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] % 2 != 0) {
+                output[index--] = input[i];
+            }
+        }
+        System.out.println("Old Array is");
+        for (int i = 0; i < input.length; i++) {
+            System.out.print(input[i] + " ");
+        }
+        System.out.println("\nNew Array after reverse is");
+      /*  for (int i = 0; i < output.length / 2; i++) {
+            int temp = output[i];
+            output[i] = output[output.length - i - 1];
+            output[output.length - i - 1] = temp;*/
+        for (int i = 0; i < output.length; i++) {
+            System.out.print(output[i] + " ");
+        }
+    }
+}
