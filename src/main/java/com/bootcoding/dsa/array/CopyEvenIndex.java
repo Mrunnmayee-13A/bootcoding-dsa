79f8d23b6e26c5582090e58dec43feda14466304
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-04-10 18:51:30 +0530

    Copy Even Array index

--- src/main/java/com/bootcoding/dsa/array/CopyEvenIndex.java
+++ src/main/java/com/bootcoding/dsa/array/CopyEvenIndex.java
@@ -2,14 +2,14 @@ package com.bootcoding.dsa.array;
 
 public class CopyEvenIndex {
     public static void main(String[] args) {
-        int input[]={3, 5, 7, 8, 10, 80};
+        int[] input ={3, 5, 7, 8, 10, 80};
         int count=0;
         for(int i=0;i<input.length;i++){
             if(i%2==0){
                 count++;
             }
         }
-        int output[]=new int[count];
+        int[] output =new int[count];
         int index=0;
         for(int i=0; i<input.length; i++){
             if(i%2==0){
