83736d66e2d84bc171484fae8a49c0621c711042
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-04-10 20:42:22 +0530

    NumbersWithEvenDigit

---
+++ src/main/java/com/bootcoding/dsa/array/NumbersWithEvenDigit.java
@@ -0,0 +1,19 @@
+package com.bootcoding.dsa.array;
+
+public class NumbersWithEvenDigit {
+    public static void main(String[] args) {
+        int[] nums = {12, 345, 2, 6, 7896};
+        int even = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] != 0) {
+                nums[i] = nums[i] / 10;
+                count++;
+            }
+            if (count % 2 == 0) {
+                even++;
+            }
+        }
+        System.out.println(even);
+    }
+}
