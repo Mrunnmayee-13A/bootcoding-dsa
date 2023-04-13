809ef6905aadc7c25bd809ea13ed1a3c87fe8be9
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-04-13 18:42:45 +0530

    StringObject

---
+++ src/main/java/com/bootcoding/dsa/string/StringObject.java
@@ -0,0 +1,9 @@
+package com.bootcoding.dsa.string;
+
+public class StringObject {
+    public static void main(String[] args) {
+        String name= "Dell";
+        String n = new String("Dell");
+        System.out.println(name);
+    }
+}
