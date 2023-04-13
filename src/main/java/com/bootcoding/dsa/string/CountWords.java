d73ae47fcbcbe4b4d8983445d6ffb21362d4fb23
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-04-13 18:43:24 +0530

    CountWords

---
+++ src/main/java/com/bootcoding/dsa/string/CountWords.java
@@ -0,0 +1,12 @@
+package com.bootcoding.dsa.string;
+
+public class CountWords {
+    public static void main(String[] args) {
+        String str = "Java is platform Independent";
+        String [] tokens= str.split(" ");
+        System.out.println(tokens.length);
+        String skills ="C, C++, java, python";
+        String [] tech = skills.split(",");
+        System.out.println("The Candidate knows "+tech.length+" Languages");
+    }
+}
