9761568d12aadaaff9cf324f2e55f59641d0410c
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-04-13 18:31:50 +0530

    LengthofString

---
+++ src/main/java/com/bootcoding/dsa/string/StringLength.java
@@ -0,0 +1,12 @@
+package com.bootcoding.dsa.string;
+
+public class StringLength {
+    public static void main(String[] args) {
+        String s ="Haldiram";
+        int len = s.length();
+        String s2 =" Haldiram!";
+        System.out.println(s2.length());
+        String s3 = "!Haldiram  ";
+        System.out.println(s3.length());
+    }
+}
