2a0e4bd72f04797041a33eb4eaa86b4b37fe97ff
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-04-13 18:41:37 +0530

    PrintChar

---
+++ src/main/java/com/bootcoding/dsa/string/CharPrint.java
@@ -0,0 +1,14 @@
+package com.bootcoding.dsa.string;
+
+public class CharPrint {
+    public static void main(String[] args) {
+        String str = "Programming";
+        int len = str.length();
+        char ch = str.charAt(len/2);
+        System.out.println(ch);
+        int r1 = str.indexOf('r');
+        System.out.println(r1);
+        int r2 =str.lastIndexOf('r');
+        System.out.println(r2);
+    }
+}
