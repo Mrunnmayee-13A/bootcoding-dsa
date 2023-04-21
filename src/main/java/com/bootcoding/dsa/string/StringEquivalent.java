575fae9ad4415e2105e00f74a2824aa96e851191
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-04-21 23:59:50 +0530

    StringEquivalent

---
+++ src/main/java/com/bootcoding/dsa/string/StringEquivalent.java
@@ -0,0 +1,16 @@
+package com.bootcoding.dsa.string;
+
+public class StringEquivalent {
+    public static void main(String[] args) {
+       String [] word1 = {"ab", "c"};
+       String [] word2 = {"a", "bc"};
+
+
+
+
+
+
+
+
+        }
+}
