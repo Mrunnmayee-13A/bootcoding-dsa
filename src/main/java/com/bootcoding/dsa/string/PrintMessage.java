4b757876bae540e7027e4637d583ad894255acbf
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-04-22 00:07:25 +0530

    PrintMessage1

--- src/main/java/com/bootcoding/dsa/string/PrintMessage.java
+++ src/main/java/com/bootcoding/dsa/string/PrintMessage.java
@@ -2,6 +2,7 @@ package com.bootcoding.dsa.string;
 
 public class PrintMessage {
     public static void main(String[] args) {
+
         System.out.println("I'am alright");
         System.out.println("I am Software Engineer in \"Google\"");
         System.out.println("Today is 14\\03\\2023");
