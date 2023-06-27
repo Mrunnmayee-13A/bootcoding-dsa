bc8cee9deb52225de8c928d04620582f99f7ae3d
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-06-27 16:59:19 +0530

    Record.java

---
+++ src/main/java/com/bootcoding/dsa/collections/arraylist/example/Record.java
@@ -0,0 +1,31 @@
+package com.bootcoding.dsa.collections.arraylist.example;
+
+public class Record {
+  String name;
+  boolean pursuing;
+  double cgpa;
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isPursuing() {
+        return pursuing;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+
+    public void setCgpa(double cgpa) {
+        this.cgpa = cgpa;
+    }
+
+    public void setPursuing(boolean pursuing) {
+        this.pursuing = pursuing;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
