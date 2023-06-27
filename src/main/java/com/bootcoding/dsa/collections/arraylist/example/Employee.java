271e0e9bd1f49d4ba88e5c39bdcdb3e32f20f1f5
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-06-27 16:47:26 +0530

    Employee.java

---
+++ src/main/java/com/bootcoding/dsa/collections/arraylist/example/Employee.java
@@ -0,0 +1,40 @@
+package com.bootcoding.dsa.collections.arraylist.example;
+
+public class Employee {
+   private String name;
+   private int experience;
+  private double salary;
+  private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
