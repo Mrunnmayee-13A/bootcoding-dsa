68aa1e9a0034b77e36b9c3cad555a915f48d2445
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-06-27 16:56:51 +0530

    Doctor.java

---
+++ src/main/java/com/bootcoding/dsa/collections/arraylist/example/Doctor.java
@@ -0,0 +1,41 @@
+package com.bootcoding.dsa.collections.arraylist.example;
+
+public class Doctor {
+  private String name;
+  private String education;
+  private int age;
+  private String  type;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
