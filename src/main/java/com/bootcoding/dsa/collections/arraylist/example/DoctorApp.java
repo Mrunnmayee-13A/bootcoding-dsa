5dcb84d993f1d350b02663c16f18629f3a17757a
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-06-28 16:03:20 +0530

    Doctor.java

--- src/main/java/com/bootcoding/dsa/collections/arraylist/example/DoctorApp.java
+++ src/main/java/com/bootcoding/dsa/collections/arraylist/example/DoctorApp.java
@@ -4,13 +4,16 @@ import java.util.ArrayList;
 
 public class DoctorApp {
     public static void main(String[] args) {
+
     DocRecord dr = new DocRecord();
     ArrayList<Doctor> doctor =dr.getDoctor(10000);
+
     for(int i=0; i< doctor.size(); i++){
         System.out.println("Name : " + doctor.get(i).getName());
         System.out.println("Education of Doctor : " + doctor.get(i).getEducation());
         System.out.println("Age : " + doctor.get(i).getAge());
         System.out.println("Type of Doctor : " +doctor.get(i).getType());
+
        }
     }
 }
