cd51c2f0d8746cb2a7268a44579aa25a8fe55a72
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-06-28 16:03:36 +0530

    Doctor.java

---
+++ src/main/java/com/bootcoding/dsa/collections/arraylist/example/SubjectMark.java
@@ -0,0 +1,32 @@
+package com.bootcoding.dsa.collections.arraylist.example;
+
+public class SubjectMark {
+    private int totalMarks;
+    private int obtainedMarks;
+    private  String subjectName;
+
+    public int getTotalMarks() {
+        return totalMarks;
+    }
+
+    public void setTotalMarks(int totalMarks) {
+        this.totalMarks = totalMarks;
+    }
+
+    public int getObtainedMarks() {
+        return obtainedMarks;
+    }
+
+    public void setObtainedMarks(int obtainedMarks) {
+        this.obtainedMarks = obtainedMarks;
+    }
+
+    public String getSubject() {
+        return subjectName;
+    }
+
+    public void setSubject(String subject) {
+        this.subjectName = subject;
+    }
+
+}
