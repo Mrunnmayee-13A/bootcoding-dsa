15b59b89a1374456b9e7b6babba516fe9cbc6795
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-07-10 00:00:10 +0530

    ArrayListEx.java

---
+++ src/main/java/com/bootcoding/dsa/collections/arraylist/example/ArrayListEx.java
@@ -0,0 +1,14 @@
+package com.bootcoding.dsa.collections.arraylist.example;
+
+import java.util.ArrayList;
+
+public class ArrayListEx {
+    public static void main(String[] args) {
+        int[] element = new int[2];
+        element [0] = 10;
+        element [1] = 20;
+        ArrayList arrayList= new ArrayList();
+
+
+    }
+}
