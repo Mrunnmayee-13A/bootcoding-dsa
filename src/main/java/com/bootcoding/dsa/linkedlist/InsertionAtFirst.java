8fec43de57fd313ee771d68d2a0bdb3ca4ccb5d6
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-07-10 00:05:27 +0530

    InsertionAtFirst

---
+++ src/main/java/com/bootcoding/dsa/linkedlist/InsertionAtFirst.java
@@ -0,0 +1,31 @@
+package com.bootcoding.dsa.linkedlist;
+
+public class InsertionAtFirst {
+    public static void main(String[] args) {
+        Node firstNode = new Node(1);
+        Node secondNode = new Node(2);
+        Node thirdNode = new Node(3);
+        Node fourthNode = new Node(4);
+
+        Node head= firstNode;
+        firstNode.next =secondNode;
+        secondNode.next =thirdNode;
+        thirdNode.next =fourthNode;
+        fourthNode.next =null;
+        Node newList = insertionAtFirst(head);
+        Create.print(newList);
+    }
+    public static Node insertionAtFirst( Node head){
+        Node newNode = new Node(5) ;
+        newNode.next = head;
+        head= newNode;
+//        Node temp= head;
+//        head = newNode;
+//        newNode.next = temp;
+
+        return head;
+
+    }
+}
+
+
