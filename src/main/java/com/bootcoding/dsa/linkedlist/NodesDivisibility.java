b5ab3c985f37ac9ecd3ce8341947741d0307c28b
Mrunnmayee-13A <mrunmayee13a@gmail.com>
2023-07-10 00:01:32 +0530

    NodesDivisibilityBy 3 or 5

---
+++ src/main/java/com/bootcoding/dsa/linkedlist/NodesDivisibility.java
@@ -0,0 +1,31 @@
+package com.bootcoding.dsa.linkedlist;
+public class NodesDivisibility {
+    public static void main(String[] args) {
+        Node firstNode = new Node(5);
+        Node secondNode = new Node(24);
+        Node thirdNode = new Node(31);
+        Node fourthNode = new Node(42);
+        Node fifthNode = new Node(63);
+
+        Node head= firstNode;
+        firstNode.next =secondNode;
+        secondNode.next =thirdNode;
+        thirdNode.next =fourthNode;
+        fourthNode.next =fifthNode;
+        fifthNode.next = null;
+
+        checkDivisible(head);
+       // Create.print(newList);
+
+    }
+
+    public static void checkDivisible(Node head){
+        Node temp= head;
+        while (temp!=null){
+            if(temp.data%3==0 || temp.data %5 ==0) {
+                System.out.println(temp.data);
+            }
+            temp =temp.next;
+        }
+    }
+}
